0a0d7f11bde2eda4952da9d06118234a872e074f
JimNouvakis <d.nouvakis@mellongroup.com>
2023-08-02 12:15:39 +0300

    v1.0.1 - Weekday.tsx - fixed its first version and styling

--- src/Components/Weekday/Weekday.tsx
+++ src/Components/Weekday/Weekday.tsx
@@ -1,11 +1,31 @@
-import {FC} from "react";
-import "./styles.css"
-const Weekday:FC = ()=>{
+import { FC } from "react";
+import "./styles.css";
+import SlotInsideWeekday from "../SlotInsideWeekday/SlotInsideWeekday";
 
-    return(
-        <div className={'WeekdayWrapper'}>Hello</div>
-    )
-}
+const Weekday: FC = () => {
+  return (
+    <div className={"WeekdayWrapper"}>
+      <div className={"day"}>
+        <p>Δευτέρα</p>
+      </div>
+      <SlotInsideWeekday
+        mealName={"ΠΡΩΙΝΟ"}
+        foodsForTheMeal={[{ text: "food" }]}
+      />
+      <SlotInsideWeekday
+        mealName={"ΓΕΥΜΑ"}
+        foodsForTheMeal={[{ text: "food" }]}
+      />
+      <SlotInsideWeekday
+        mealName={"ΑΠΟΓΕΥΜΑΤΙΝΟ"}
+        foodsForTheMeal={[{ text: "food" }]}
+      />
+      <SlotInsideWeekday
+        mealName={"ΒΡΑΔΙΝΟ"}
+        foodsForTheMeal={[{ text: "food" }]}
+      />
+    </div>
+  );
+};
 
-
-export default Weekday
+export default Weekday;
