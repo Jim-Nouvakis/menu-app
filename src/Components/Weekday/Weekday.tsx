1c75db4f7f44fd6adbd46aca575ebd4fc4608d22
JimNouvakis <d.nouvakis@mellongroup.com>
2023-08-02 09:56:09 +0300

    v1.0.0 - First components added

---
+++ src/Components/Weekday/Weekday.tsx
@@ -0,0 +1,11 @@
+import {FC} from "react";
+import "./styles.css"
+const Weekday:FC = ()=>{
+
+    return(
+        <div className={'WeekdayWrapper'}>Hello</div>
+    )
+}
+
+
+export default Weekday
---
+++ src/Dashboard/Dashboard.tsx
@@ -0,0 +1,9 @@
+import React,{FC} from 'react'
+import "./styles.css"
+import Weekday from "../Components/Weekday/Weekday";
+const Dashboard:FC = () =>{
+    return(<div><Weekday/></div>)
+}
+
+
+export default Dashboard
