4d86649f4e5a106b584665503bdf678d2836a2e4
JimNouvakis <d.nouvakis@mellongroup.com>
2023-08-02 14:43:46 +0300

    v1.0.4 - store.ts, counter-slice.ts, hooks.ts - first dummy version of redux created

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,19 @@ import React from "react";
 import "./App.css";
 import Dashboard from "./Dashboard/Dashboard";
 import SettingsDashboard from "./SettingsDashboard/SettingsDashboard";
+import { useAppDispatch, useAppSelector } from "./app/hooks";
+import { incremented } from "./features/counter/counter-slice";
 
 function App() {
+  const count = useAppSelector((state) => state.counter.value);
+  const dispatch = useAppDispatch();
   return (
     <div className={"App"}>
+      <button
+        onClick={() => {
+          dispatch(incremented());
+        }}
+      />
       <Dashboard />
       <SettingsDashboard />
     </div>
---
+++ src/features/counter/counter-slice.ts
@@ -0,0 +1,23 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface CounterState {
+  value: number;
+}
+
+const initialState: CounterState = {
+  value: 0,
+};
+
+const counterSlice = createSlice({
+  name: "Counter",
+  initialState,
+  reducers: {
+    incremented(state) {
+      state.value += 1;
+    },
+  },
+});
+
+export const { incremented } = counterSlice.actions;
+
+export default counterSlice.reducer;
